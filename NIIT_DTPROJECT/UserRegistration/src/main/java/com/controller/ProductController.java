ef4efa3903add42f8ecc60f247622608c8390dea
selvi <selvianu@gmail.com>
2018-02-20 15:33:15 +0530

    cart partially completed

--- NIIT_DTPROJECT/UserRegistration/src/main/java/com/controller/ProductController.java
+++ NIIT_DTPROJECT/UserRegistration/src/main/java/com/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
-import java.nio.file.Files;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -10,151 +11,96 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dt.dao.CategoryDao;
 import com.dt.dao.ProductDao;
 import com.dt.dao.SupplierDao;
-import com.dt.dao.UserDao;
-import com.dt.model.Category;
 import com.dt.model.Product;
 
 @Controller
 public class ProductController {
-
 	@Autowired
-	private ProductDao productDao;
+	ProductDao productDao;
 	@Autowired
-	private CategoryDao categoryDao;
+	CategoryDao categoryDao;
 	@Autowired
-	private UserDao userDao;
+	SupplierDao supplierDao;
 	@Autowired
-	private SupplierDao supplierDao;
+	HttpServletRequest request;
 
-	/*
-	 * @RequestMapping("/addproduct") public String AddProduct(Model model) {
-	 * System.out.println("Product Controller"); return "addproduct"; }
-	 */
+	@RequestMapping(value = { "/saveproduct" })
+	public String saveProduct(@ModelAttribute("pro") Product product) {
+		productDao.insertProduct(product);
+		MultipartFile image = product.getImage();
+		String imagepath = request.getServletContext().getRealPath("/resource/images");
+		System.out.println("---------Directory:-------" + imagepath);
+		Path path = Paths.get(imagepath + File.separator + product.getPid() + ".jpg");
+		System.out.println("-------Path:-----" + path.toString());
 
-	@RequestMapping("/showProductDetails")
-	public String showProducts() {
-		System.out.println("show products - Product Controller");
-		return "/showProducts";
-	}
-
-	@RequestMapping(value = "/admin/addProduct", method = RequestMethod.POST)
-	public String addProduct(HttpServletRequest req, RedirectAttributes redirectAttributes,
-			@RequestParam("imgName") MultipartFile file, Model model) {
-		Product p = new Product();
-		System.out.println("in controller");
-		model.addAttribute("category", categoryDao.retrieveCategory());
-		String UPLOADED_FOLDER = "C:\\Users\\selvi\\Pictures\\DT-images";
-		p.setPname(req.getParameter("pname"));
-		p.setDesc(req.getParameter("desc"));
-		// Float pprice = Float.parseFloat("price");
-		p.setPrice(Float.parseFloat(req.getParameter("price")));
-		p.setStockAvailable(Integer.parseInt(req.getParameter("stockAvailable")));
-		Category cat = new Category();
-		// cat.setCid(Integer.parseInt(req.getParameter("selectCategory")));
-		// p.setCategory(cat);
-
-		String filepath = req.getSession().getServletContext().getRealPath("/");
-		String filename = file.getOriginalFilename();
-		p.setImgName(filename);
-		productDao.addProduct(p);
-		if (file.isEmpty()) {
-			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-			return "addProduct";
-		}
-		// productDaoImpl.addProduct(p);
 		try {
-			byte[] imagebytes = file.getBytes();
-			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-			Files.write(path, imagebytes);
-			redirectAttributes.addFlashAttribute("message",
-					"You successfully uploaded '" + file.getOriginalFilename() + "'");
-		} catch (Exception e) {
+			image.transferTo(new File(path.toString()));
+		} catch (IllegalStateException e) {
 
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
 		}
-		System.out.println("product - security added");
-		return "index";
+		return "redirect:/productlist";
 	}
 
-	@RequestMapping(value = "/showProducts")
-	public String listAllProducts(Model m) {
-		System.out.println("to show product - List");
-		List<Product> listProducts = productDao.getAllProducts();
-		m.addAttribute("productList", listProducts);
-		/*
-		 * for (Product product : listProducts) {
-		 * System.out.println(product.getPname()); }
-		 */
-		return "showProducts";
+	@RequestMapping(value = { "/productlist" })
+	public String showAllProduct(Model model) {
+		List<Product> product = productDao.getAllProducts();
+		model.addAttribute("product", product);
+		return "productlist";
 	}
 
 	@RequestMapping(value = "/admin/deleteProduct/{pid}")
 	public String deleteProduct(@PathVariable int pid) {
-		System.out.println("to delect specified product by id");
 		productDao.deleteProduct(pid);
-		return "redirect:/showProducts";
+		return "redirect:/productlist";
 	}
 
 	@RequestMapping(value = { "/all/product/viewProduct/{pid}" })
 	public String viewProduct(@PathVariable int pid, Model model) {
-		System.out.println("to view specified product by id");
-		Product product = productDao.getProduct(pid);
+		Product product = productDao.getProductById(pid);
 		model.addAttribute("product", product);
-		System.out.println(product);
-		return "viewProduct";
+		return "viewproduct";
 	}
 
-	@RequestMapping(value = "/showCategory")
-	public String listAllCategory(Model m) {
-		System.out.println("product list");
-		System.out.println("to show product - List");
-		List<Category> listCategory = categoryDao.getAllCategory();
-		m.addAttribute("categoryList", listCategory);
-		for (Category category : listCategory) {
-			System.out.println(category.getCname());
-		}
-		return "catList";
-	}
-
-	@RequestMapping(value = "/showCategoryByPid")
-	public String ProductsByPid(Model m, @RequestParam("pid") int pid) {
-		System.out.println("product list");
-		System.out.println("to show product - List");
-		List<Category> listCategoryByPid = productDao.getProductBypid(pid);
-		m.addAttribute("productListById", listCategoryByPid);
-		for (Category category : listCategoryByPid) {
-			System.out.println(category.getCname());
-		}
-		return "catList";
-	}
-
-	@RequestMapping(value = { "/all/product/updateProduct/{pid}" })
+	@RequestMapping(value = { "/admin/product/editform/{pid}" })
 	public String editForm(@PathVariable int pid, Model model) {
-		model.addAttribute("cat", categoryDao.getAllCategory());
-		// model.addAttribute("supp", supplierDao.getAllSuppliers());
-		Product product = productDao.getProduct(pid);
+		model.addAttribute("category", categoryDao.getAllCategories());
+		model.addAttribute("supplier", supplierDao.getAllSuppliers());
+		Product product = productDao.getProductById(pid);
 		model.addAttribute("products", product);
-		return "updateProduct";
+		return "updateproduct";
 	}
 
-	/*
-	 * @RequestMapping(value = "/showProducts", method = RequestMethod.POST) public
-	 * String showPdt(@ModelAttribute("SpringWeb") Product pdt, ModelMap model) {
-	 * model.addAttribute("name", pdt.getPname()); model.addAttribute("desc",
-	 * pdt.getDesc()); // model.addAttribute("id", student.getId()); return
-	 * "result"; }
-	 */
+	@RequestMapping(value = { "/all/product/updateproduct" })
+	public String updateSupplier(@ModelAttribute("products") Product product) {
+		productDao.updateProduct(product);
+		return "redirect:/productlist";
+	}
 
+	@RequestMapping(value = { "/returns" })
+	public String backToProducts() {
+		System.out.println("Back to Product List");
+		// Product product = productDao.getProductById(pid);
+		// model.addAttribute("product", product);
+		return "productlist";
+	}
+
+	@RequestMapping(value = { "/returns2" })
+	public String backToHome() {
+		System.out.println("Back to Home");
+		// Product product = productDao.getProductById(pid);
+		// model.addAttribute("product", product);
+		return "home";
+	}
 }
--- NIIT_DTPROJECT/UserRegistration/src/main/java/com/controller/UserController.java
+++ NIIT_DTPROJECT/UserRegistration/src/main/java/com/controller/UserController.java
@@ -1,82 +1,44 @@
 package com.controller;
 
-import java.io.IOException;
-import java.security.Principal;
-import java.sql.Date;
-import java.sql.SQLException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.dt.dao.CategoryDao;
+import com.dt.dao.SupplierDao;
 import com.dt.dao.UserDao;
+import com.dt.model.Category;
+import com.dt.model.Product;
+import com.dt.model.Supplier;
 import com.dt.model.User;
 
 @Controller
 public class UserController {
 	@Autowired
-	private UserDao userDao;
+	UserDao userDao;
+	@Autowired
+	CategoryDao categoryDao;
+	@Autowired
+	SupplierDao supplierDao;
 
-	@RequestMapping("/register")
-	public String register() {
+	@RequestMapping(value = { "/register" })
+	public String registerPage(Model model) {
+		model.addAttribute("supp", new Supplier());
+		model.addAttribute("pro", new Product());
+		model.addAttribute("cat", new Category());
+		model.addAttribute("category", categoryDao.getAllCategories());
+		model.addAttribute("supplier", supplierDao.getAllSuppliers());
+		model.addAttribute("user", new User());
 		return "register";
 	}
 
-	@RequestMapping(value = "/registrationController", method = RequestMethod.POST)
-	public String register(@RequestParam("username") String username, @RequestParam("password1") String password1,
-			@RequestParam("password2") String password2, @RequestParam("email") String email,
-			@RequestParam("dob") Date dob, ModelMap model, HttpServletResponse res)
-			throws ClassNotFoundException, SQLException, IOException {
-		System.out.println("registration contoller");
-		User user = new User();
-		user.setUsername(username);
-		user.setPassword1(password1);
-		user.setPassword2(password2);
-		user.setEmail(email);
-		user.setDob(dob);
-		user.setRole("role_user");
-		user.setEnabled(true);
-		if (password1.equals(password2)) {
-			userDao.addUser(user);
-			System.out.println(username + " added");
-		} else {
-			System.out.println("password mismatch");
-			// res.sendRedirect("passwordincorect");
-		}
-		// res.sendRedirect("register");
-		return "login";
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-
-	public String login(@RequestParam("username") String username, @RequestParam("password") String password,
-			Model model) throws ClassNotFoundException, SQLException {
-		System.out.println("user Controller");
-		User user2 = userDao.getUser(username);
-		System.out.println(user2.getUsername());
-		if (user2.getPassword1().equals(password)) {
-			System.out.println(user2.getUsername() + "User Page");
-		}
-		return "login";
-	}
-
-	@RequestMapping(value = "/admin/login", method = RequestMethod.POST)
-	public String login(ModelMap model, Principal principal) {
-		//System.out.println("principal name" + principal.getName());
-		// String name = principal.getName();
-		System.out.println("spring security login");
-		// model.addAttribute("username", name);
-		model.addAttribute("username");
-		// model.addAttribute("spring security");
-		return "index";
-
-	}
+	/*@RequestMapping("/userLogged")
+	public String check() {
+		return "home";
+	}*/
 
 	@RequestMapping(value = "/login")
 	public String loginPage(@RequestParam(required = false) boolean Login, Model model) {
@@ -85,6 +47,16 @@ public class UserController {
 		} else {
 			model.addAttribute("error", null);
 		}
-		return "login";
+		return "Login";
 	}
+
+	@RequestMapping(value = { "/saveregister" })
+	public String saveRegister(@ModelAttribute("user") User user) {
+		user.setEnabled(true);
+		user.setRole("ROLE_USER");
+		userDao.insertValues(user);
+		System.out.println("user information gets inserted");
+		return "Login";
+	}
+
 }
