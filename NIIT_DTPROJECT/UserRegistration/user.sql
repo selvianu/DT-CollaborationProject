bb7d036e3416adb23fa6a500c242136bff5526c6
selvi <selvianu@gmail.com>
2018-03-20 12:41:47 +0530

    backend project

--- NIIT_DTPROJECT/UserRegistration/user.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE userdetails (username VARCHAR(100),password1 VARCHAR(20),password2 VARCHAR(20),email VARCHAR(50),dob DATE);
-INSERT INTO userdetails VALUES("selvi","nithin","nithin","selvi@gmail.com","2000-08-10");
-SELECT * FROM userdetails WHERE username="niit";
